5f0fbbaa5d9ece0ec7b41584f11ba4b0a372f544
Saad <alami.saad.pro@gmail.com>
2019-04-19 16:19:13 +0200

    ajout TP3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {PokemonListComponent} from './pokemons/pokemon-list/pokemon-list.component';
+import {MatButtonModule, MatCardModule, MatChipsModule, MatGridListModule, MatIconModule, MatListModule} from '@angular/material';
+import {PokemonDetailComponent} from './pokemons/pokemon-detail/pokemon-detail.component';
+import {PokemonsModule} from './pokemons/pokemons.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    PokemonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
